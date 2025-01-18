4773d23712d3640394e17d5b317eaa3a49ef38d6
Pranav Sarode <sarodepranav2001@gmail.com>
2025-01-18 18:11:58 +0530

    login form react code in progress

--- login-form/src/components/form-input-row.jsx
+++ login-form/src/components/form-input-row.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-
 class FormInputRow extends Component {
+    msg = "Johnny Bravo";
     render() {
         return <>
             <div class="form-row">
@@ -8,7 +8,7 @@ class FormInputRow extends Component {
                     <label for={this.props.nameId}> {this.props.label} </label>
                 </div>
                 <div class="form-row-input">
-                    <input type={this.props.inputType} id={this.props.nameId} name={this.props.nameId} class="input-max-width-500px" />
+                    <input value={this.msg} type={this.props.inputType} id={this.props.nameId} name={this.props.nameId} class="input-max-width-500px" />
                 </div>
             </div>
         </>;
--- login-form/src/components/form-row-submit-btn.jsx
+++ login-form/src/components/form-row-submit-btn.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 
 class FormRowSubmitBtn extends Component {
+     onLoginSubmit : {
+        console.log(document.getElementById("username"));
+        alert('Response Submitted');
+      },
     render() {
+        
         return <>
             <div class="form-row">
-                <div class="submit-btn">
-                    Submit
+                <div class="submit-btn" onClick={onLoginSubmit()}>
+                    Submit 
                 </div>
             </div>
         </>;
