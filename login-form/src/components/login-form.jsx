254372a43e12d6b4829e63a113d97ec8cc276a78
Pranav Sarode <sarodepranav2001@gmail.com>
2025-01-21 12:58:09 +0530

    Updated login form file

---
+++ login-form/src/components/login-form.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import FormInputRow from './form-input-row';
+import FormRowSubmitBtn from './form-row-submit-btn';
+import Header from './header';
+
+class LoginForm extends Component {
+    render() {
+
+        return <>
+            <div className="main-content">
+                <Header />
+                <div className='form-content'>
+                    <FormInputRow nameId="username" label="Username" inputType="text" />
+                    <FormInputRow nameId="password" label="Password" inputType="password" />
+                    <FormRowSubmitBtn />
+                    <Link to="/forget">Forget Password</Link>
+                </div>
+            </div>
+        </>;
+    }
+}
+
+export default LoginForm;
+
